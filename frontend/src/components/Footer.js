7938a1360bbb9c9b9ce1767b3f70331e5c0fb6ae
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-07-30 21:08:10 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200'>
+    <footer className='bg-slate-100'>
       <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Amit</p>
+       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Biman</p>
       </div>
     </footer>
   )
