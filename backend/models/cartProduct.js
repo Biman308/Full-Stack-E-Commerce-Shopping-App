7a4983bd62846f23b4d257c44d77aee63fe91983
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-07-31 11:01:07 +0530

    Update cartProduct.js

--- backend/models/cartProduct.js
+++ backend/models/cartProduct.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const addToCart = mongoose.Schema({
-   productId : {
-        ref : 'product',
-        type : String,
-   },
-   quantity : Number,
-   userId : String,
-},{
-    timestamps : true
-})
+// const addToCart = mongoose.Schema({
+//    productId : {
+//         ref : 'product',
+//         type : String,
+//    },
+//    quantity : Number,
+//    userId : String,
+// },{
+//     timestamps : true
+// })
 
 
-const addToCartModel = mongoose.model("addToCart",addToCart)
+// const addToCartModel = mongoose.model("addToCart",addToCart)
 
-module.exports = addToCartModel
+// module.exports = addToCartModel
